153fdb2b46432234e0db322aac8798af20488c15
TruongGiangBui <truonggiangbui9@gmail.com>
2021-04-18 14:10:10 +0700

    a

--- src/routes/site.js
+++ src/routes/site.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const newController = require('../app/controllers/SiteController');
 
-            router.use('/search', newController.search);
+        router.use('/search', newController.search);
             router.use('/', newController.index);
 module.exports = router;
